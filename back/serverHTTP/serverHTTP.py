5e69aacc33e73f958204aeec7d5e2671e1f76b8b
Pierre <p.bregeard57@gmail.com>
2023-10-21 20:09:23 +0200

    Ajout de la db et connexion avec

--- back/serverHTTP/serverHTTP.py
+++ back/serverHTTP/serverHTTP.py
@@ -1,13 +1,27 @@
 from flask import Flask
+import pymongo
 
 app = Flask(__name__)
 
+uri = "mongodb://mongo:27017"
+mongo_client = pymongo.MongoClient(uri)
+db = mongo_client["vroomlegend"]
+
 
 @app.route('/')
-def hello():
-    return {
-        "message": "Hello, World!"
-    }
+def test1():
+    collection = db["user"]
+    data = {"email": "test@gmail.com", "mdp": "wow"}
+    collection.insert_one(data)
+    return "pass"
+
+
+@app.route('/find')
+def test2():
+    collection = db["user"]
+    for doc in collection.find({"email": "test@gmail.com"}):
+        print(doc)
+    return "pass"
 
 
 if __name__ == "__main__":
