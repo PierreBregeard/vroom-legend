50a4807641187b8a146f6880a2f7d28fde9b4a17
Michel <michel.recchia3@etu.univ-lorraine.fr>
2023-11-01 10:30:47 +0100

    Feature : création d'une map

--- client/src/main.py
+++ client/src/main.py
@@ -1,17 +1,35 @@
 import pygame
+import pyscroll
+import pytmx
 
 pygame.init()
-window = pygame.display.set_mode((500, 500))
+
+screenHeight = 500
+screenWidth = 700
+screenSize = (screenWidth, screenWidth)
+
+window = pygame.display.set_mode(screenSize)
 clock = pygame.time.Clock()
 
 rect = pygame.Rect(0, 0, 20, 20)
 rect.center = window.get_rect().center
+
+# Initialisation de la map en format .tmx
+tmx_data = pytmx.util_pygame.load_pygame("../Maps/FirstMap.tmx")
+map_data = pyscroll.data.TiledMapData(tmx_data)
+map_layer = pyscroll.orthographic.BufferedRenderer(map_data, screenSize)
+
+# Dessiner le groupe de calques
+group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+
 
 vel = 5
 
 run = True
 while run:
     clock.tick(60)
+    group.draw(window)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -25,9 +43,7 @@ while run:
 
     rect.x %= window.get_width()
     rect.y %= window.get_height()
-
-    window.fill(0)
-    pygame.draw.rect(window, (0, 0, 0), rect)
+    pygame.draw.rect(window, (255, 0, 0), rect)
     pygame.display.flip()
 
 pygame.quit()
