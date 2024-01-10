8b6c431c10e6b6e1b5a72230cca4a0f5e497ef51
Michel <michel.recchia1@gmail.com>
2024-01-10 20:28:53 +0100

    Rock zone on map

--- client/src/classes/HUD/CheckpointManager.py
+++ client/src/classes/HUD/CheckpointManager.py
@@ -1,16 +1,17 @@
 from functools import reduce
 
 import pygame
-
+from .Font import Font
 
 class CheckpointManager:
 
     def __init__(self, screen_size):
         self.screen_size = screen_size
+        self.my_font = Font.get_font(20)
 
     def checkpoint_missed_alert(self, window):
-        my_font = pygame.font.SysFont('arialblack', 20)
-        text_surface = my_font.render('You missed a checkpoint, go back !', False, (0, 0, 0))
+
+        text_surface = self.my_font.render('You missed a checkpoint, go back !', False, (0, 0, 0))
         text_size = text_surface.get_size()
         window.blit(text_surface, (self.screen_size[0]/2 - text_size[0]/2, self.screen_size[1]/2 - text_size[1]/2 - 40))
 
@@ -25,7 +26,6 @@ class CheckpointManager:
         checkpoint_length = len(checkpoint_list)
         checkpoint_passed = reduce(lambda x, element: x + 1, filtered, 0)
 
-        my_font = pygame.font.SysFont('arialblack', 20)
-        text_surface = my_font.render(f'Checkpoints {checkpoint_passed}/{checkpoint_length}', False, (0, 0, 0))
+        text_surface = self.my_font.render(f'Checkpoints {checkpoint_passed}/{checkpoint_length}', False, (0, 0, 0))
         text_size = text_surface.get_size()
-        window.blit(text_surface, (self.screen_size[0]-text_size[0], 0))
+        window.blit(text_surface, (self.screen_size[0]-text_size[0]-20, 20))
