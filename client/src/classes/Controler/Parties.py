a673e8f2d6d2e04e8546d509a1ff6c3652a27563
MOLINA Eva <evamolina@live.fr>
2024-01-11 16:35:38 +0100

    fix

--- client/src/classes/Controler/Parties.py
+++ client/src/classes/Controler/Parties.py
@@ -1,13 +1,18 @@
 import requests
+from threading import Thread
 
 
 class ControlerParties:
 
     @staticmethod
     def save_history(data):
-        try:
-            response = requests.post("http://127.0.0.1:5000/saveHistory", json=data)
-            print(response.content)
-            return response.content
-        except requests.exceptions.ConnectionError:
-            return None
+
+        def f():
+            try:
+                requests.post("http://127.0.0.1:5000/saveHistory", json=data)
+                return
+            except requests.exceptions.ConnectionError:
+                return
+
+        thread = Thread(target=f)
+        thread.start()
